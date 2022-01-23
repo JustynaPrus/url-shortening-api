dcd39c1d366f10c8f5b571ae518a65c92feb43ff
Justyna Pruś <justyna.p.prus@gmail.com>
2022-01-23 11:52:40 +0100

    mapping list

--- src/components/ShortenUrl.js
+++ src/components/ShortenUrl.js
@@ -5,8 +5,8 @@ const ShortenUrl = () => {
   const [value, setValue] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [url, setUrl] = useState([]);
-  const [shorten, setShorten] = useState([]);
+  const [data, setData] = useState({});
+  const [list, setList] = useState([]);
 
   const API = `https://api.shrtco.de/v2/shorten?url=${value}`;
 
@@ -14,26 +14,27 @@ const ShortenUrl = () => {
     setLoading(true);
     await fetch(API)
       .then((response) => response.json())
-      .then((data) => {
-        const newUrl = data.result.full_short_link;
-        setShorten((shorten) => [...shorten, newUrl]);
-      })
+      .then((data) => setData(data.result))
       .catch((error) => {
         setError(error);
       })
       .finally(() => {
         setLoading(false);
       });
-    return { loading, error };
+    return { data, loading, error };
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     loadData();
     console.log(value);
-    setUrl((url) => [...url, value]);
-    console.log(shorten);
+    console.log(data);
     setValue("");
+  };
+  console.log(data);
+
+  const handleClick = () => {
+    setList([...list, data]);
   };
 
   return (
@@ -45,23 +46,25 @@ const ShortenUrl = () => {
             type="text"
             placeholder="Shorten a link here..."
             value={value}
-            onChange={(e) => setValue((value) => [...value, e.target.value])}
+            onChange={(e) => setValue(e.target.value)}
           />
-          <button type="submit">Shorten It!</button>
+          <button type="submit" onClick={handleClick}>
+            Shorten It!
+          </button>
         </form>
       </StyledSection>
       <Wrapper>
-        {shorten.map((item) => (
-          <li>
-            <p>{item}</p>
-          </li>
-        ))}
-        {url.map((item) => (
-          <li>
-            <p>{item}</p>
-            <button>Copy</button>
-          </li>
-        ))}
+        <ul>
+          <p>{data.original_link}</p>
+          <p>{data.full_short_link}</p>
+          {list.map((item) => (
+            <li>
+              <p>{item.original_link}</p>
+              <p>{item.full_short_link}</p>
+              <button>Copy</button>
+            </li>
+          ))}
+        </ul>
       </Wrapper>
     </>
   );
